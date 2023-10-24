a2a81f3a57a400af5be127c082d23a58efd8be58
flyinLemon <75799201+sameerihs@users.noreply.github.com>
2023-10-24 21:43:37 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import React from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="header bg-primary text-white py-5 text-center">
+        <h1 className="name">Mohamed Sameer</h1>
+        <p className="job-title">Web and App Developer</p>
+      </header>
+
+      <section className="about py-5">
+        <div className="container">
+          <h2 className="section-title">About Me</h2>
+          <p>
+            I'm a passionate Flutter Developer with expertise in Dart,
+            JavaScript, React, Node.js, and more. I enjoy creating web
+            applications and solving complex problems.
+          </p>
+        </div>
+      </section>
+
+      <section className="contact py-5 bg-primary text-white">
+        <div className="container">
+          <h2 className="section-title text-white">Contact Me</h2>
+          <p>Email: mohamed.sameer2021@vitstudent.ac.in</p>
+          <p>
+            LinkedIn:{" "}
+            <a
+              href="https://www.linkedin.com/in/mohamed-sameer-44455021b/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              LinkedIn Profile
+            </a>
+          </p>
+          <p>
+            GitHub:{" "}
+            <a
+              href="https://github.com/sameerihs"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              GitHub Profile
+            </a>
+          </p>
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default App;
